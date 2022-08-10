58d7c4c2d8fea4c03fe7d9e811b8261adac765d8
Suzanne Mozes <suzanne.mozes@gmail.com>
2022-08-10 07:42:35 -0400

    edit schema

--- models/Fabrics.js
+++ models/Fabrics.js
@@ -2,14 +2,24 @@ const mongoose = require('mongoose')
 
 const fabricsSchema = new mongoose.Schema({
   name:  { type: String, required: true },
+  length:  { type: String, required: false },
+  width:  { type: String, required: false },
   color:  { type: String, required: true },
   color2:  { type: String, required: false },
   color3:  { type: String, required: false },
-  description: { type: String, required: true },
-  category
+  description: { type: String, required: false },
+  category: { type: String, required: true },
+  category2:  { type: String, required: false },
+  category3:  { type: String, required: false },
   image:  { type: Image, required: true },
-  color:  { type: String, required: true },
-  washed: Boolean
+  image2:  { type: Image, required: false },
+  isbn:  { type: String, required: false },
+  stock:  { type: String, required: true },
+  tag1:  { type: String, required: false },
+  tag2:  { type: String, required: false },
+  tag3:  { type: String, required: false },
+  price:  { type: String, required: true },
+
 });
 
 const Fabrics = mongoose.model('Fabric', fabricSchema);
@@ -23,7 +33,7 @@ name:'linen',
     description: 'tk'
     category: '' //required
     subcategory: '' // not required
-    washed:true
+
     img:
     isbn: "tk"
     tags: 
